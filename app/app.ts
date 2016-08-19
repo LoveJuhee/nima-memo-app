067b4c0d55e02e87ed1150df359059e386ccfd70
hyounwoo.ko <lj1212.kit@gmail.com>
2016-08-19 16:02:51 +0900

    LoginPage가 첫번째로 뜨도록 구현

--- app/app.ts
+++ app/app.ts
@@ -1,18 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, provide} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {TabsPage} from './pages/tabs/tabs';
-
+import {LoginPage} from './pages';
+import {AppStorage, UserStorage} from './providers';
 
 @Component({
   template: '<ion-nav [root]="rootPage"></ion-nav>'
 })
-export class MyApp {
+export class App {
 
   private rootPage: any;
 
   constructor(private platform: Platform) {
-    this.rootPage = TabsPage;
+    this.rootPage = LoginPage;
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -22,4 +23,8 @@ export class MyApp {
   }
 }
 
-ionicBootstrap(MyApp);
+let providers: any[] = [
+  provide('AppStorage', { useClass: AppStorage }),
+  provide('UserStorage', { useClass: UserStorage }),
+];
+ionicBootstrap(App, providers);
